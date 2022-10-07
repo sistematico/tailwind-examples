3a7a28fe83566aacbd8920b91da92008641fb8e8
Lucas Saliés Brum <sistematico@gmail.com>
2022-10-07 18:17:24 -0400

    Update automático

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+// const mode = process.env.APP_ENV
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/tailwind-examples/' : '/',
+  base: process.env.MODE == 'production' ? '/tailwind-examples/' : '/',
   plugins: [vue()],
   resolve:{
     alias:{
